a9dabb6fe37edb3539b623624a06b98d4413c7f0
Yon Hawer Piñeros <96067867+yohapimo@users.noreply.github.com>
2022-02-08 22:46:20 -0500

    texto

--- tallerBuenasPracticas/src/tallerbuenaspracticas/TallerBuenasPracticas.java
+++ tallerBuenasPracticas/src/tallerbuenaspracticas/TallerBuenasPracticas.java
@@ -16,7 +16,7 @@ public class TallerBuenasPracticas {
      */
     public static void main(String[] args) {
         
-        System.out.println("hola mundo");
+        System.out.println("hola mundo prueba 3");
         
     }
     
